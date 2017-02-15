840e47a0ebd0f93a76b9481635a91e7fabaa2157
ShuriK97 <salash.a.s.work@gmail.com>
2017-02-16 00:19:43 +0300

    Some activities
    Full MainScreen, beginning of Forum.

---
+++ ImperiumRomanum/app/src/main/java/com/example/alexander/imperiumromanum/MainScreen.java
@@ -0,0 +1,13 @@
+package com.example.alexander.imperiumromanum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
